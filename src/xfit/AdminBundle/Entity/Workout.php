1d0a0847b80f383fecfe8d1275c80e89c7b65af6
Kyle Partidge <partkyle@gmail.com>
2011-11-16 19:15:39 -0600

    created basic workout model

---
+++ src/xfit/AdminBundle/Entity/Workout.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace xfit\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * xfit\AdminBundle\Entity\Workout
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Workout
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var text $ante
+     *
+     * @ORM\Column(name="ante", type="text")
+     */
+    private $ante;
+
+    /**
+     * @var text $buy_in
+     *
+     * @ORM\Column(name="buy_in", type="text")
+     */
+    private $buy_in;
+
+    /**
+     * @var text $cash_out
+     *
+     * @ORM\Column(name="cash_out", type="text")
+     */
+    private $cash_out;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set ante
+     *
+     * @param text $ante
+     */
+    public function setAnte($ante)
+    {
+        $this->ante = $ante;
+    }
+
+    /**
+     * Get ante
+     *
+     * @return text 
+     */
+    public function getAnte()
+    {
+        return $this->ante;
+    }
+
+    /**
+     * Set buy_in
+     *
+     * @param text $buyIn
+     */
+    public function setBuyIn($buyIn)
+    {
+        $this->buy_in = $buyIn;
+    }
+
+    /**
+     * Get buy_in
+     *
+     * @return text 
+     */
+    public function getBuyIn()
+    {
+        return $this->buy_in;
+    }
+
+    /**
+     * Set cash_out
+     *
+     * @param text $cashOut
+     */
+    public function setCashOut($cashOut)
+    {
+        $this->cash_out = $cashOut;
+    }
+
+    /**
+     * Get cash_out
+     *
+     * @return text 
+     */
+    public function getCashOut()
+    {
+        return $this->cash_out;
+    }
+}
